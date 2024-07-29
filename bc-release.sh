22fbb7a60e16dc50598d7e61736c49d52f5a3ed6
Colin Drake <github@accounts.colindrake.me>
2024-07-29 12:27:03 -0500

    Add target to release

--- bc-release.sh
+++ bc-release.sh
@@ -13,4 +13,5 @@ zip -r $ZIP_PATH dist/
 # Submit release to Github.
 gh release create $GIT_COMMIT $ZIP_PATH \
   --repo bandcampdotcom/j2objc \
-  --generate-notes
+  --generate-notes \
+  --target bc
