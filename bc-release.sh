f118874ceafa5f336f4488ed7f9bd1b979e3d642
Colin Drake <github@accounts.colindrake.me>
2024-07-29 12:30:16 -0500

    Update target

--- bc-release.sh
+++ bc-release.sh
@@ -3,7 +3,8 @@
 set -e
 
 # Setup file names.
-GIT_COMMIT=`git rev-parse --short HEAD`
+# Note our commit is based on `master`, not the `bc` branch.
+GIT_COMMIT=`git rev-parse --short master`
 ZIP_PATH=j2objc-dist-$GIT_COMMIT.zip
 
 # Create zip file for release.
@@ -11,7 +12,8 @@ rm -f $ZIP_PATH
 zip -r $ZIP_PATH dist/
 
 # Submit release to Github.
+# Note our target is master. This will link to the master branch rather than our fork.
 gh release create $GIT_COMMIT $ZIP_PATH \
   --repo bandcampdotcom/j2objc \
   --generate-notes \
-  --target bc
+  --target master
